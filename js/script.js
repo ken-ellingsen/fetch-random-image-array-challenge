a3eb761e6af17a1d14a892d29d7a958bf46b35d3
Ken Ellingsen <152531539+ken-ellingsen@users.noreply.github.com>
2024-02-14 16:33:45 -0600

    Added image selection randomizer and button functionality to display random images

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-async function getImage(){
+async function getImage() {
     const res = await fetch("https://picsum.photos/v2/list?limit=100")
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getImage();
+function selectRandomImage(images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+}
+
+function displayImage(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function () {
+    getImage();
+});
